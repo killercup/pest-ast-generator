daf47560da57b3fa4f3288f5f14b0933d9cd03ff
Pascal Hertleif <pascal@technocreatives.com>
2019-06-20 13:58:19 +0200

    Generate and even test this stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![recursion_limit = "512"]
+
 pub use self::error::Error;
 use heck::{CamelCase, SnakeCase};
 use pest_meta::{
@@ -5,16 +7,17 @@ use pest_meta::{
     parser::{self, consume_rules, parse},
 };
 use proc_macro2::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 use snafu::ResultExt;
 
 mod ast_helpers;
 
-pub fn grammar_to_rust(grammar: &str) -> Result<TokenStream, Error> {
+pub fn grammar_to_rust(name: &str, grammar: &str) -> Result<TokenStream, Error> {
+    let parser_type = ident(&name.to_camel_case())?;
     let tokens = parse(parser::Rule::grammar_rules, grammar).context(error::ParseGrammar)?;
     let ast = consume_rules(tokens).map_err(|errors| Error::ReadGrammar { errors })?;
 
-    let rules_as_types = dbg!(ast)
+    let rules_as_types = ast
         .into_iter()
         .filter(|rule| rule.ty != RuleType::Silent)
         .map(rule_to_rust)
@@ -22,6 +25,10 @@ pub fn grammar_to_rust(grammar: &str) -> Result<TokenStream, Error> {
 
     Ok(quote! {
         #rules_as_types
+
+        #[derive(Debug, Clone, pest_derive::Parser)]
+        #[grammar_inline = #grammar]
+        pub struct #parser_type;
 
         pub(crate) fn span_into_str(span: pest::Span) -> &str {
             span.as_str()
@@ -37,12 +44,15 @@ fn rule_to_rust(rule: Rule) -> Result<TokenStream, Error> {
 
     Ok(match rule.expr {
         Expr::Str(..) | Expr::Insens(..) => {
+            let impl_from_pest = impl_from_pest_for_unit_struct(&type_name, ident("Rule")?, name);
+
             quote! {
-                #[derive(Debug, Clone, Copy, PartialEq, Eq, FromPest)]
-                #[pest_ast(rule(Rule::#name))]
+                #[derive(Debug, Clone, Copy, PartialEq, Eq)]
                 pub struct #type_name<'src> {
                     marker: core::marker::PhantomData<&'src str>,
                 }
+
+                #impl_from_pest
             }
         }
         Expr::Seq(a, b) => {
@@ -60,7 +70,7 @@ fn rule_to_rust(rule: Rule) -> Result<TokenStream, Error> {
                 .collect::<Vec<TokenStream>>();
 
             quote! {
-                #[derive(Debug, Clone, PartialEq, Eq, FromPest)]
+                #[derive(Debug, Clone, PartialEq, Eq, pest_ast::FromPest)]
                 #[pest_ast(rule(Rule::#name))]
                 pub struct #type_name<'src> {
                     #(
@@ -102,7 +112,7 @@ fn rule_to_rust(rule: Rule) -> Result<TokenStream, Error> {
             }
 
             quote! {
-                #[derive(Debug, Clone, PartialEq, Eq, FromPest)]
+                #[derive(Debug, Clone, PartialEq, Eq, pest_ast::FromPest)]
                 #[pest_ast(rule(Rule::#name))]
                 pub enum #type_name<'src> {
                     #(
@@ -112,7 +122,7 @@ fn rule_to_rust(rule: Rule) -> Result<TokenStream, Error> {
             }
         }
         _ => quote! {
-            #[derive(Debug, Clone, PartialEq, Eq, FromPest)]
+            #[derive(Debug, Clone, PartialEq, Eq, pest_ast::FromPest)]
             #[pest_ast(rule(Rule::#name))]
             pub struct #type_name<'src> {
                 #[pest_ast(outer(with(span_into_str)))]
@@ -143,5 +153,40 @@ mod error {
         Unsupported { description: &'static str },
         #[snafu(display("Code generation failed: {}", errors.iter().map(Error::to_string).join("\n")))]
         CodeGenerationErrors { errors: Vec<Error> },
+    }
+}
+
+fn impl_from_pest_for_unit_struct(
+    type_name: impl ToTokens,
+    rule_type: impl ToTokens,
+    rule_name: impl ToTokens,
+) -> TokenStream {
+    quote! {
+        impl<'a> ::from_pest::FromPest<'a> for #type_name<'a> {
+            type Rule = #rule_type;
+            type FatalError = ::from_pest::Void;
+
+            fn from_pest(
+                pest: &mut ::from_pest::pest::iterators::Pairs<'a, Self::Rule>,
+            ) -> ::std::result::Result<Self, ::from_pest::ConversionError<::from_pest::Void>>
+            {
+                let mut clone = pest.clone();
+                let pair = clone.next().ok_or(::from_pest::ConversionError::NoMatch)?;
+                if pair.as_rule() == #rule_type :: #rule_name {
+                    let mut inner = pair.into_inner();
+                    let inner = &mut inner;
+                    let this = #type_name { marker: core::marker::PhantomData };
+                    if inner.clone().next().is_some() {
+                        Err(::from_pest::ConversionError::Extraneous {
+                            current_node: "KeyModeReplace",
+                        })?;
+                    }
+                    *pest = clone;
+                    Ok(this)
+                } else {
+                    Err(::from_pest::ConversionError::NoMatch)
+                }
+            }
+        }
     }
 }
--- tests/simple.rs
+++ tests/simple.rs
@@ -1,15 +1,44 @@
 #[test]
 fn simple1() {
-    let grammar = r#"
-        foo = { a | b | c }
-        a = { "a" }
-        b = { "b" }
-        c = { "foo" ~ integer ~ "bar" }
-        integer = @{ (ASCII_DIGIT){1,10} }
-    "#;
-    println!("{}", rustfmt(pest_ast_generator::grammar_to_rust(grammar).unwrap()));
+    compile(
+        "simple1",
+        r#" foo = { a | b | c }
+            a = { "a" }
+            b = { "b" }
+            c = { "foo" ~ integer ~ "bar" }
+            integer = @{ (ASCII_DIGIT){1,10} }"#,
+    )
+    .unwrap();
 }
 
-fn rustfmt(tokens: proc_macro2::TokenStream) -> String {
-    duct::cmd!("rustfmt").input(tokens.to_string()).read().unwrap()
+fn compile(name: &str, grammar: &str) -> Result<(), Box<dyn std::error::Error>> {
+    use std::{fs, path::PathBuf, process::Command};
+
+    let _ = env_logger::builder().is_test(true).filter(None, log::LevelFilter::Debug).try_init();
+
+    let target_dir = PathBuf::from("./tests/test-package");
+    let example_dir = target_dir.join("examples");
+    fs::create_dir_all(&example_dir)?;
+
+    let mut rust_code = pest_ast_generator::grammar_to_rust(name, grammar)?.to_string();
+    rust_code.push_str("\n\n fn main() {}");
+
+    fs::write(example_dir.join(name).with_extension("rs"), rust_code)?;
+
+    let fmt = Command::new("cargo").args(&["fmt", "--all"]).current_dir(&target_dir).status()?;
+
+    if !fmt.success() {
+        Err("cargo fmt failed")?;
+    }
+
+    let fmt = std::process::Command::new("cargo")
+        .args(&["check", "--example", &name])
+        .current_dir(&target_dir)
+        .status()?;
+
+    if !fmt.success() {
+        Err("cargo check failed")?;
+    }
+
+    Ok(())
 }
