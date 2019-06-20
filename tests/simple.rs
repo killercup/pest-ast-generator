80dc9880afb40c04ca1d42741e58ebaf9d440855
Pascal Hertleif <pascal@technocreatives.com>
2019-06-20 14:43:44 +0200

    refactor test setup

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,44 +1,11 @@
-#[test]
-fn simple1() {
-    compile(
-        "simple1",
-        r#" foo = { a | b | c }
-            a = { "a" }
-            b = { "b" }
-            c = { "foo" ~ integer ~ "bar" }
-            integer = @{ (ASCII_DIGIT){1,10} }"#,
-    )
-    .unwrap();
-}
+#[macro_use] mod utils;
 
-fn compile(name: &str, grammar: &str) -> Result<(), Box<dyn std::error::Error>> {
-    use std::{fs, path::PathBuf, process::Command};
+test!(capture: "integer = @{ ASCII_DIGIT+ }");
 
-    let _ = env_logger::builder().is_test(true).filter(None, log::LevelFilter::Debug).try_init();
-
-    let target_dir = PathBuf::from("./tests/test-package");
-    let example_dir = target_dir.join("examples");
-    fs::create_dir_all(&example_dir)?;
-
-    let mut rust_code = pest_ast_generator::grammar_to_rust(name, grammar)?.to_string();
-    rust_code.push_str("\n\n fn main() {}");
-
-    fs::write(example_dir.join(name).with_extension("rs"), rust_code)?;
-
-    let fmt = Command::new("cargo").args(&["fmt", "--all"]).current_dir(&target_dir).status()?;
-
-    if !fmt.success() {
-        Err("cargo fmt failed")?;
-    }
-
-    let fmt = std::process::Command::new("cargo")
-        .args(&["check", "--example", &name])
-        .current_dir(&target_dir)
-        .status()?;
-
-    if !fmt.success() {
-        Err("cargo check failed")?;
-    }
-
-    Ok(())
-}
+test!(simple_choice_and_seq: r#"
+    foo = { a | b | c }
+    a = { "a" }
+    b = { "b" }
+    c = { "foo" ~ integer ~ "bar" }
+    integer = @{ ASCII_DIGIT+ }
+"#);
---
+++ tests/utils.rs
@@ -0,0 +1,42 @@
+#![allow(unused)]
+
+macro_rules! test {
+    ($name:ident : $grammar:expr) => {
+        #[test]
+        fn $name() {
+            crate::utils::compile(stringify!($name), $grammar).unwrap();
+        }
+    }
+}
+
+pub fn compile(name: &str, grammar: &str) -> Result<(), Box<dyn std::error::Error>> {
+    use std::{fs, path::PathBuf, process::Command};
+
+    let _ = env_logger::builder().is_test(true).filter(None, log::LevelFilter::Debug).try_init();
+
+    let target_dir = PathBuf::from("./tests/test-package");
+    let example_dir = target_dir.join("examples");
+    fs::create_dir_all(&example_dir)?;
+
+    let mut rust_code = pest_ast_generator::grammar_to_rust(name, grammar)?.to_string();
+    rust_code.push_str("\n\n fn main() {}");
+
+    fs::write(example_dir.join(name).with_extension("rs"), rust_code)?;
+
+    let fmt = Command::new("cargo").args(&["fmt", "--all"]).current_dir(&target_dir).status()?;
+
+    if !fmt.success() {
+        Err("cargo fmt failed")?;
+    }
+
+    let fmt = std::process::Command::new("cargo")
+        .args(&["check", "--example", &name])
+        .current_dir(&target_dir)
+        .status()?;
+
+    if !fmt.success() {
+        Err("cargo check failed")?;
+    }
+
+    Ok(())
+}
